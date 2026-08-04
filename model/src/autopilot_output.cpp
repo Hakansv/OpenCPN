6a8d7bee3e6bf10817409f1987e6e5d3639f3d21
Hakan <momo@stupan.se>
2026-08-04 21:31:27 +0200

    Dashb, AP Edit APB brg to origin dest

--- model/src/autopilot_output.cpp
+++ model/src/autopilot_output.cpp
@@ -195,13 +195,8 @@ bool UpdateAutopilotN0183(Routeman &routeman) {
     if (wp30DevData[0] != 0) {  // flag: updated file
       int brg = routeman.GetCurrentBrgToActivePoint();
       if (brg < 361 && brg >= 0) {
-        // Adjust what's sent by deviation //  and Var. - No not that 2026-07-17
-        // f_brg += std::isnan(gVar) ? wp30DevData[brg] : wp30DevData[brg] -
-        // gVar;
         f_brg += wp30DevData[brg];
       }
-      //} else {  // No deviation data  // Take out? Bad thought?
-      //  f_brg -= std::isnan(gVar) ? 0.0 : gVar;
     }
     if (f_brg > 360.0)
       f_brg -= 360.;
@@ -257,6 +252,14 @@ bool UpdateAutopilotN0183(Routeman &routeman) {
                             routeman.GetpActiveRouteSegmentBeginPoint()->m_lat,
                             routeman.GetpActiveRouteSegmentBeginPoint()->m_lon,
                             &brg1, &dist1);
+    // Test Hakan
+    if (g_bXTE_multiply) {
+      if (!devfileNotfound && wp30DevData[0] != 0) {
+        brg1 += wp30DevData[int(brg1)];
+      }
+    }
+
+    // << Hakan
 
     if (g_bMagneticAPB && !std::isnan(gVar)) {
       double brg1m =
